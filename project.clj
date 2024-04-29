571afee33546445858f95856c86241b185fe17f8
Karol Wójcik <ceo@blockether.io>
2024-04-29 11:02:22 +0200

    Bump coordinates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/tools.logging "1.1.1-SNAPSHOT"
+(defproject io.github.fierycod/tools.logging "1.1.2"
   :description "Clojure logging API."
   :url "https://github.com/clojure/tools.logging"
   :license {:name "Eclipse Public License"
